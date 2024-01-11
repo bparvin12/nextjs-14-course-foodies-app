fccb36ba755f638832c3676100af5dd6958e2702
Bijan Parvin <bparvin12@gmail.com>
2024-01-11 12:20:05 -0800

    feat: initial commit for nextjs 14 course foodies app

---
+++ components/main-header/main-header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+// Components
+import logoImg from '@/assets/logo.png';
+import MainHeaderBackground from '@/components/main-header/main-header-background';
+
+// Styles
+import classes from './main-header.module.css';
+import NavLink from './nav-link';
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href='/'>
+          <Image src={logoImg} alt='A plate with food on it' priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodies Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
